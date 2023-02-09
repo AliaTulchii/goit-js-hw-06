ebcba2595428d3b55b080adbb49b087645bbb1df
Halina Tulchii <halendeihalyna@gmail.com>
2023-02-09 22:17:24 +0200

    tasks

--- js/task-06.js
+++ js/task-06.js
@@ -4,13 +4,18 @@ const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
     textInput.textContent = event.currentTarget.value;
     if (textInput.value.length === 6) {
-        textInput.classList.toggle("invalid");
-        textInput.classList.toggle("invalid");
-        textInput.classList.add("valid");
-    } else if (textInput.value.length < 6 || textInput.value.length > 6 ) {
+        textInput.classList.toggle("valid");
+        textInput.classList.toggle("invalid");     
+       
+               
+
+    } else if (textInput.value.length !== 6 ) {
         textInput.classList.toggle("invalid");
         
-    }
+       
+    }  
+
+   
     
 });
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputValue = document.querySelector('#controls input');
+let boxes = document.querySelector('#boxes');
+let newDiv = document.createElement('div');
+
+
+
+function createBoxes(amount) {
+ 
+
+
+}
+
+function createDiv ()
+  {
+  var boxEle = document.createElement('div');
+  var container = document.querySelector('.app');
+  boxEle.style.width = "100px";
+  boxEle.style.height = "100px";
+  boxEle.style.backgroundColor = '#f00';
+  container.appendChild(boxEle);
+  }
+
+
+
+
+
+// <div id="controls">
+//       <input type="number" min="1" max="100" step="1" />
+//       <button type="button" data-create>Create</button>
+//       <button type="button" data-destroy>Destroy</button>
+//     </div>
+
+//     <div id="boxes"></div>
