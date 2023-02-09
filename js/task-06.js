322604bf74427ac82c5c6c549210bb4b3f821d2f
Halina Tulchii <halendeihalyna@gmail.com>
2023-02-09 21:29:06 +0200

    add new tasks

--- js/task-06.js
+++ js/task-06.js
@@ -4,9 +4,12 @@ const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
     textInput.textContent = event.currentTarget.value;
     if (textInput.value.length === 6) {
+        textInput.classList.toggle("invalid");
+        textInput.classList.toggle("invalid");
         textInput.classList.add("valid");
     } else if (textInput.value.length < 6 || textInput.value.length > 6 ) {
-        textInput.classList.add("invalid");
+        textInput.classList.toggle("invalid");
+        
     }
     
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,30 +1,8 @@
-const sizeControl = document.getElementById('#font-size-control');
-const text = document.getElementById('#text');
+const sizeControl = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+sizeControl.value = 20;
 
-// function changeSizeBySlider(sizeControl.min, sizeControl.max) {
-    
-    
-//     // Set slider value as fontSize
-//     text.style.fontSize = sizeControl.min;
-//     }
-
-
-// <input type="range" min="10" max="30" id="slider"
-// onchange="changeSizeBySlider()" value="20" />
-// </center>
-
-// <script>
-// var cont = document.getElementById("container");
-
-// function changeSizeByBtn(size) {
-
-// // Set value of the parameter as fontSize
-// cont.style.fontSize = size;
-// }
-
-// function changeSizeBySlider() {
-// var slider = document.getElementById("slider");
-
-// // Set slider value as fontSize
-// cont.style.fontSize = slider.value;
-// }
+sizeControl.addEventListener('input', function () {
+    const sizeValue = sizeControl.value;
+    text.style.fontSize = sizeValue + "px";
+})
