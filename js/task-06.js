c170c5aaf35819ac3e91dd8c9bc2c707070374ee
Halina Tulchii <halendeihalyna@gmail.com>
2023-02-10 09:32:20 +0200

    update

--- js/task-06.js
+++ js/task-06.js
@@ -4,18 +4,21 @@ const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", (event) => {
     textInput.textContent = event.currentTarget.value;
     if (textInput.value.length === 6) {
-        textInput.classList.toggle("valid");
-        textInput.classList.toggle("invalid");     
+        textInput.classList.add("valid");
+        textInput.classList.replace("invalid", "valid");
+        
        
                
 
-    } else if (textInput.value.length !== 6 ) {
-        textInput.classList.toggle("invalid");
+    } else if (textInput.value.length !== 6) {
+        textInput.classList.add("invalid");        
+        textInput.classList.replace("valid", "invalid");
         
        
-    }  
+    } 
+    
 
-   
+    
     
 });
 
